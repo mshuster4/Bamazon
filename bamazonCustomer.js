c01e5beeaabad878040632b842cab482fc606673
Emroze <emrozesd@users.noreply.github.com>
2019-02-23 16:00:55 -0800

    added print table logic

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,32 @@
-//Prompt user with:
-    // Asking for ID of product
-    // How many units of product
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
-// Check if Store has enough
-// Log if not
-// Store does, fulfill order 
-// Update sql showing remaining quantity
-// Once update goes through, show customer total cost of their purchase
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "Margys04041990!",
+    database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id" + connection.threadId);
+    printAllproducts();
+});
+
+function printAllproducts() {
+    connection.query("SELECT * FROM products", function(err, res){
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].id + " | " + res[i].product_name + " | " 
+                        + res[i].department_name + " | " + res[i].price
+                        + " | " + res[i].stock_quantity);
+        }
+
+        console.log("-----------------------------------");
+
+    });
+}
--- bamazonSeeds.sql
+++ bamazonSeeds.sql
@@ -4,19 +4,46 @@ CREATE DATABASE bamazon_db;
 
 USE bamazon_db;
 
-CREATE TABLE productions (
-    item_id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE products (
+    id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(100) NOT NULL,
-    department_name VARCHAR(45) NOT NULL,
-    price DECIMAL (10,2) NULL,
-
+    department_name VARCHAR(100) NOT NULL,
+    price DECIMAL(10,4) NOT NULL,
+    stock_quantity INT(100) NOT NULL,
+    PRIMARY KEY(id)
 );
 
--- create table with:
- -- item_id
- -- product name
- -- department name
- -- price (cost to customer)
- -- stock quality 
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Mac Laptop", "Electronics", 200, 3);
 
--- populate database with about 10 different products
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES  ("Flannel Shirt", "Clothing", 45.75, 9);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Coffe Mug", "Home & Kitchen", 3.50, 88);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Ugly Christmas Sweater", "Clothing", 50.99, 340);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Combat Boots", "Shoes", 350, 8);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("JBL Speakers", "Electronics", 450.50, 2);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Honeywell Air Conditioner", "Electronics", 666.66, 1);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Blackout Curtains", "Bed & Bath", 29, 24);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("LED Lamp", "Electronics", 18, 40);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("World Map", "Office Products", 15, 3);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Ipod Shuffle", "Electronics", 0, 0);
+
+SELECT * FROM products; 
